c48198fd259b260c79ef57b58b73159bc58275d1
Kevin Tindall <kevinkjt2000@gmail.com>
2017-05-14 11:20:36 -0500

    fix volume up/down commands

--- config.def.h
+++ config.def.h
@@ -64,8 +64,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *upvol[]    = { "amixer", "set", "Master", "5+",     NULL };
-static const char *downvol[]  = { "amixer", "set", "Master", "5-",     NULL };
+static const char *upvol[]    = { "amixer", "set", "Master", "5%+",     NULL };
+static const char *downvol[]  = { "amixer", "set", "Master", "5%-",     NULL };
 static const char *mutevol[]  = { "amixer", "set", "Master", "toggle", NULL };
 
 static Key keys[] = {
